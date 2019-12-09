acdc7eb847a61c0cdc395798e5b43dfde540bc4c
Wenyuan Wu <wenyuan.wu@uzh.ch>
2019-12-09 03:07:58 +0100

    topic modeling finished

---
+++ topic_modeling.py
@@ -0,0 +1,147 @@
+import numpy as np
+import pandas as pd
+
+import gensim
+import gensim.corpora as corpora
+from gensim.utils import simple_preprocess
+from gensim.models import CoherenceModel
+
+import spacy
+from spacy.lemmatizer import Lemmatizer
+from spacy.lang.de.stop_words import STOP_WORDS
+import de_core_news_md
+
+from tqdm import tqdm_notebook as tqdm
+from pprint import pprint
+
+
+quellen = pd.read_csv('quellen.csv')
+quellen_doc = quellen['content']
+
+nlp = spacy.load("de_core_news_md")
+
+# My list of stop words.
+stop_list = ["fur", "mal", "all", "’s", "l.", "Herr", "xxxxx", "etc"]
+
+# Updates spaCy's default stop words list with my additional words.
+nlp.Defaults.stop_words.update(stop_list)
+
+# Iterates over the words in the stop words list and resets the "is_stop" flag.
+for word in STOP_WORDS:
+    lexeme = nlp.vocab[word]
+    lexeme.is_stop = True
+
+
+def lemmatizer(doc):
+    # This takes in a doc of tokens from the NER and lemmatizes them.
+    # Pronouns (like "I" and "you" get lemmatized to '-PRON-', so I'm removing those.
+    doc = [token.lemma_ for token in doc if token.lemma_ != '-PRON-']
+    doc = u' '.join(doc)
+    return nlp.make_doc(doc)
+
+
+def remove_stopwords(doc):
+    # This will remove stopwords and punctuation.
+    # Use token.text to return strings, which we'll need for Gensim.
+    doc = [token.text for token in doc if token.is_stop != True and token.is_punct != True]
+    return doc
+
+
+# The add_pipe function appends our functions to the default pipeline.
+nlp.add_pipe(lemmatizer, name='lemmatizer', after='ner')
+nlp.add_pipe(remove_stopwords, name="stopwords", last=True)
+
+doc_list = []
+# Iterates through each article in the corpus.
+for doc in tqdm(quellen_doc):
+    # Passes that article through the pipeline and adds to a new list.
+    pr = nlp(doc)
+    doc_list.append(pr)
+
+# print(doc_list)
+
+# Creates, which is a mapping of word IDs to words.
+words = corpora.Dictionary(doc_list)
+
+# Turns each document into a bag of words.
+corpus = [words.doc2bow(doc) for doc in doc_list]
+
+lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus,
+                                            id2word=words,
+                                            num_topics=10,
+                                            random_state=2,
+                                            update_every=1,
+                                            passes=10,
+                                            alpha='auto',
+                                            per_word_topics=True)
+
+pprint(lda_model.print_topics())
+
+
+def format_topics_sentences(ldamodel=None, corpus=corpus, texts=quellen_doc):
+    # Init output
+    sent_topics_df = pd.DataFrame()
+
+    # Get main topic in each document
+    for i, row_list in enumerate(ldamodel[corpus]):
+        row = row_list[0] if ldamodel.per_word_topics else row_list
+        # print(row)
+        row = sorted(row, key=lambda x: (x[1]), reverse=True)
+        # Get the Dominant topic, Perc Contribution and Keywords for each document
+        for j, (topic_num, prop_topic) in enumerate(row):
+            if j == 0:  # => dominant topic
+                wp = ldamodel.show_topic(topic_num)
+                topic_keywords = ", ".join([word for word, prop in wp])
+                sent_topics_df = sent_topics_df.append(pd.Series([int(topic_num), round(prop_topic,4), topic_keywords]), ignore_index=True)
+            else:
+                break
+    sent_topics_df.columns = ['Dominant_Topic', 'Perc_Contribution', 'Topic_Keywords']
+
+    # Add original text to the end of the output
+    contents = pd.Series(texts)
+    sent_topics_df = pd.concat([sent_topics_df, contents], axis=1)
+    return(sent_topics_df)
+
+
+df_topic_sents_keywords = format_topics_sentences(ldamodel=lda_model, corpus=corpus, texts=doc_list)
+
+# Format
+df_dominant_topic = df_topic_sents_keywords.reset_index()
+df_dominant_topic.columns = ['Document_No', 'Dominant_Topic', 'Topic_Perc_Contrib', 'Keywords', 'Text']
+df_dominant_topic[['Document_No', 'Dominant_Topic', 'Topic_Perc_Contrib', 'Keywords']].to_csv('dominant_topic.csv')
+
+
+# 1. Wordcloud of Top N words in each topic
+from matplotlib import pyplot as plt
+from wordcloud import WordCloud, STOPWORDS
+import matplotlib.colors as mcolors
+
+cols = [color for name, color in mcolors.TABLEAU_COLORS.items()]  # more colors: 'mcolors.XKCD_COLORS'
+
+cloud = WordCloud(stopwords=stop_list,
+                  background_color='white',
+                  width=2500,
+                  height=1800,
+                  max_words=10,
+                  colormap='tab10',
+                  color_func=lambda *args, **kwargs: cols[i],
+                  prefer_horizontal=1.0)
+
+topics = lda_model.show_topics(formatted=False)
+
+fig, axes = plt.subplots(2, 2, figsize=(10,10), sharex=True, sharey=True)
+
+for i, ax in enumerate(axes.flatten()):
+    fig.add_subplot(ax)
+    topic_words = dict(topics[i][1])
+    cloud.generate_from_frequencies(topic_words, max_font_size=300)
+    plt.gca().imshow(cloud)
+    plt.gca().set_title('Topic ' + str(i), fontdict=dict(size=16))
+    plt.gca().axis('off')
+
+
+plt.subplots_adjust(wspace=0, hspace=0)
+plt.axis('off')
+plt.margins(x=0, y=0)
+plt.tight_layout()
+plt.show()
